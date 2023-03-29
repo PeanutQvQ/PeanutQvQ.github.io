4df0ed468ef8ac58978c99f0c1a67b357a9a3af2
PeanutQvQ <129055095+PeanutQvQ@users.noreply.github.com>
2023-03-29 21:40:42 +0800

    Add files via upload

---
+++ Practise.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+//  #define Max 10000
+// enum Sex {
+//     Male,
+//     Female,
+//     Secret,
+// };
+// int main () {
+//     enum Sex s = Female;
+//     printf ("%d\n", Male);
+//     return 0;
+// }
+int main() {
+    // char arr1[] = "abc";
+    // char arr2[3] = {'a', 'b', 'c'};
+    // char arr3[] = {'a', 'b', 'c', '\0'};
+    // printf ("%s\n%s\n%s\n", arr1, arr2, arr3);
+    // printf ("%d\n", strlen (arr1));
+    // printf ("%d\n", strlen (arr2));
+    // printf ("%d\n", strlen (arr3));
+    // printf ("%C\n%C\n", '\130', '\x30'); // x 0
+    printf ("%d\n", strlen ("c:\test\08\test.c"));
+    return 0;
+}
